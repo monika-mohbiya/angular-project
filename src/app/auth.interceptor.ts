b198bafe5b7f0513dbab3bee481ecfa5523b9f33
Monika <monikamohbiya777@gmail.com>
2025-11-19 11:35:46 +0530

    latest Code

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,17 +1,51 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
+import { jwtDecode } from 'jwt-decode';
+import { CommonService } from './services/common.service';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem("Access Token:")
+  const token = localStorage.getItem("Access Token:");
+  const loader = inject(CommonService);
+  const router = inject(Router);
+
+  // ✅ Loader start
+  loader.show();
+
   if (token) {
-    const cloned = req.clone({
+    try {
+      const decoded: any = jwtDecode(token);
+      const expiry = decoded.exp * 1000; // convert to ms
+
+      if (Date.now() > expiry) {
+        // Token expired → logout + redirect
+        localStorage.removeItem('Access Token:');
+        loader.hide();
+        router.navigate(['/login']);
+        return next(req); // stop API call
+      }
+    } catch (error) {
+      console.log("Invalid token");
+      localStorage.removeItem('Access Token:');
+      loader.hide();
+      router.navigate(['/login']);
+      return next(req);
+    }
+  }
+  // ✅ Set header only if token exists
+  const clonedReq = token
+    ? req.clone({
       setHeaders: {
         'Authorization': token,
         'Content-Type': 'application/json',
       }
     })
-    return next(cloned);
-  } else {
-    return next(req);
-  }
+    : req;
+
+  // ✅ Send request and hide loader after response
+  return next(clonedReq).pipe(
+    finalize(() => loader.hide())
+  );
 
 };
--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -5,6 +5,13 @@ import { computed, Injectable, signal } from '@angular/core';
 })
 export class CommonService {
   selectedId = signal<number | null>(null);
+  loading = signal(false);
   constructor() { }
+  show() {
+    this.loading.set(true);
+  }
 
+  hide() {
+    this.loading.set(false);
+  }
 }
